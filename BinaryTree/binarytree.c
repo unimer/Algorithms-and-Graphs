94eaf3ddce2cd9569dcc5129e53da9f97ec464be
Nikola <nikolamarin@protonmail.com>
2019-06-01 20:03:10 +0200

    in-order traversal implemented

--- BinaryTree/binarytree.c
+++ BinaryTree/binarytree.c
@@ -8,26 +8,78 @@ typedef struct node{
 }node_t;
 
 
-void insert(struct node **root, int value){
+void init_tree(struct node **root, int value){
     struct node *current = *root;
+    // initialize root node if root = NULL
     if(!current){
         current = malloc(sizeof(struct node));
         current->value = value;
         *root = current;
     }
+
 }
 
+struct node* new_node(int value){
+    struct node *new = NULL;
+    new = malloc(sizeof(struct node));
+    new->value = value;
+    new->left = NULL;
+    new->right = NULL;
+    return new;
+}
+
+/*
+ * Function: insert
+ * Arguments: struct node **, value
+ * Inserts new node into binary tree
+ */ 
+void insert(struct node *nd, int value){
+    
+    if(nd->value > value){
+        if(!nd->left){
+            nd->left = new_node(value);
+        }
+        else{
+            insert(nd->left, value);
+        }
+    }
+    else if(nd->value < value){
+        if(!nd->right){
+            nd->right = new_node(value);
+        }
+        else{
+            insert(nd->right, value);
+        }
+    }
+}
+
+void in_order(struct node *nd){
+    if(nd){
+        in_order(nd->left);
+        printf("%d \n", nd->value);
+        in_order(nd->right);
+    }
+}
+
+void as_is(struct node *nd){
+
+
+}
 
 void main(){
     struct node *root = NULL;
     int test_numbers[10] = {54,10,25,91,85,40,42,50,58,78};
     
-    insert(&root,10);
-    if(!root){
-        printf("oo\n");
+    for(int i=0; i<10; i++){   
+        if(!root){
+            init_tree(&root, test_numbers[i]);
+        }
+        else{
+            insert(root, test_numbers[i]);
+        }
     }
-    printf("%d\n", root->value);
 
-
+    printf("------------------In Order Traversal-------------------\n");
+    in_order(root);
     
 }
